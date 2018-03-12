de4f4296c88f3c8802dadbceb42eb167701ef6b9
wanghaoxun <wanghaoxun@guazi.com>
2018-03-12 11:36:30 +0800

    test embedded sub module

---
+++ moduleone/src/main/java/com/nelson/moduleone/ModuleOneTest.java
@@ -0,0 +1,13 @@
+package com.nelson.moduleone;
+
+/**
+ * Created by Nelson on 2018/3/12.
+ */
+
+public class ModuleOneTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am from module one!");
+    }
+
+}
---
+++ submoduleone/src/main/java/com/nelson/submoduleone/SubModuleOneTest.java
@@ -0,0 +1,13 @@
+package com.nelson.submoduleone;
+
+/**
+ * Created by Nelson on 2018/3/12.
+ */
+
+public class SubModuleOneTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, I am from sub module one!");
+    }
+
+}
